a7777bf69c033a6b6dcc19e82dd00c4599b4008b
Marie Lucas <ria093@gmail.com>
2014-09-09 18:01:28 -0700

    set up Yelp API; search by zipcode; no formatting of results

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
+  resources :places, only: :index
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
