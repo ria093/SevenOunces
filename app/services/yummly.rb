89478d860c54180f312ef6a6c8fc24dc34a79cb8
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2014-09-27 15:48:59 -0700

    Update cached content

--- app/services/yummly.rb
+++ app/services/yummly.rb
@@ -19,20 +19,21 @@ class Yummly
   end
 
   def self.search(recipe_name)
-    page = rand(500)
-    Rails.cache.fetch("yummly-page-#{page}", expires_in: 1.day) {
-      recipes_response = Faraday.get('http://api.yummly.com/v1/api/recipes',
+    page = rand(1..40)
+    recipes_response = Rails.cache.fetch("yummly-page-#{page}", expires_in: 1.day) {
+      Faraday.get('http://api.yummly.com/v1/api/recipes',
                                      _app_id: ENV['YUMMLY_APP_ID'],
                                      _app_key: ENV['YUMMLY_APP_KEY'],
                                      q: recipe_name,
                                      maxResult: 12,
                                      start: page,
-                                     requirePictures: true)
-      JSON.parse(recipes_response.body)['matches'].map do |recipe_json|
-        SearchResult.new(recipe_json['recipeName'], recipe_json['rating'], recipe_json['totalTimeInSeconds'],
-                         recipe_json['ingredients'], display_img_url(recipe_json['smallImageUrls'].try(:first)), recipe_json['id'])
-      end
+                                     requirePictures: true).body
     }
+
+    JSON.parse(recipes_response)['matches'].map do |recipe_json|
+      SearchResult.new(recipe_json['recipeName'], recipe_json['rating'], recipe_json['totalTimeInSeconds'],
+                       recipe_json['ingredients'], display_img_url(recipe_json['smallImageUrls'].try(:first)), recipe_json['id'])
+    end
   end
 
   private
